16dbe92fa5aab7dc2580ed0163a14fa6aa1669fb
shivam15102005 <159278628+shivam15102005@users.noreply.github.com>
2024-10-03 23:58:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect("mongodb://localhost:27017/blogify")
+  .connect("mongodb://localhost:127.0.0.1:27017/blogify")
   .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
